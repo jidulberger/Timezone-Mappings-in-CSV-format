530952c26754d1bc064c1d20ff90646b987a24c4
Gloria <gloriajw@localhost.localdomain>
2011-12-20 22:47:02 -0500

    added zip codes to us cities

--- gen_tz_city.sh
+++ gen_tz_city.sh
@@ -10,6 +10,12 @@ do
    state=`echo $line | awk -F, '{print $2}'`
    olson=`echo $line | awk -F, '{print $3}'`
 
+   city_upper=`echo $city | tr '[:lower:]' '[:upper:]'`
+   zips=`grep "${city_upper}" ./free-zipcode-database.csv | grep \"${state}\" | awk -F, '{print $1}' | sed 's/\"//g'`
+
+   for z in $zips
+   do
+
    if test "$counter" -ge 2
    then
    echo ","
@@ -23,10 +29,13 @@ do
          \"city\": \"${city}\",
          \"state_abbrev\": \"${state}\",
          \"olson_timezone\": \"${olson}\"
+         \"zipcode\": \"${z}\"
       }
    }
    "
    counter=$(( $counter + 1 ))
+
+   done
 
 done < ~/Timezone-Mappings-in-CSV-format/us_cities_to_tz.csv
 
